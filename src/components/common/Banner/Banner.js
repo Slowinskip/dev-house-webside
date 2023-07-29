bde5f08a2b00a7d1573a5e64a3b0d455ca55be1b
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-29 16:53:04 +0200

    Add banner components with html and css

--- src/components/common/Banner/Banner.js
+++ src/components/common/Banner/Banner.js
@@ -1,7 +1,22 @@
 import React from "react";
 import styles from "./Banner.module.scss";
+import { Col, Row } from "react-bootstrap";
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className={styles.bannerDiv}>
+      <Row className={styles.banner_row}>
+        <Col xs={12} md={6} className={styles.banner_bg_1}>
+          <div>
+            <h1>Dev House</h1>
+            <h1>Your place on earth</h1>
+          </div>
+        </Col>
+        <Col xs={12} md={6} className={styles.banner_bg_2}>
+          1
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/layout/Home/Home.js
+++ src/components/layout/Home/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./Home.module.scss";
+import Banner from "../../common/Banner/Banner";
 const Home = () => {
-  return <div>Home</div>;
+  return <Banner />;
 };
 
 export default Home;
