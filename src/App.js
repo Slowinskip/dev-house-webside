853a922db71dee62ab1ab277a0cf425b9fd39fd9
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-07-31 22:22:46 +0200

    Reset project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
-import Footer from "./components/layout/Footer/Footer";
-import Home from "./components/layout/Home/Home";
-import Navbar from "./components/layout/Navbar/Navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
-  );
+  return <>Dev House </>;
 }
 
 export default App;
