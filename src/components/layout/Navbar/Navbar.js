252ce7b201fbf523235ba8c628f60f8bc7737a68
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-08-01 12:18:50 +0200

    Add frame motion to navbar

--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -1,28 +1,66 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
 import { Col, Container, Row } from "react-bootstrap";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delay: 1,
+        staggerChildren: 0.5,
+        when: "beforeChildren",
+      },
+    },
+  };
+  const item = {
+    hidden: { x: 200, opacity: 0 },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
   return (
     <Container>
       <Row className={styles.navbarContainer}>
         <Col>
           {" "}
-          <h1>
+          <motion.h1
+            initial={{ x: 200, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.5, duration: 1 }}
+          >
             Dev<span>House</span>
-          </h1>
+          </motion.h1>
         </Col>
-        <Col className={"" + styles.links_container}>
-          <Link className={styles.links} to="/">
-            Build with Us
-          </Link>
-          <Link className={styles.links} to="/">
-            Develop with Us
-          </Link>
-          <Link className={styles.links} to="/">
-            About
-          </Link>
-        </Col>
+        <motion.Col
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className={"" + styles.links_container}
+        >
+          <motion.li variants={item}>
+            <Link variants={item} className={styles.links} href="/">
+              Build with Us
+            </Link>
+          </motion.li>
+          <motion.li variants={item}>
+            <Link className={styles.links} href="/">
+              Develop with Us
+            </Link>
+          </motion.li>
+          <motion.li variants={item}>
+            <Link variants={item} className={styles.links} href="/">
+              About
+            </Link>
+          </motion.li>
+        </motion.Col>
       </Row>{" "}
     </Container>
   );
