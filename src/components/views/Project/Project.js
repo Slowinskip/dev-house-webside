79342a190d83c815ec00c81e2fe1a5764554d2df
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-08-05 18:49:15 +0200

    Add frame motion to project componen

--- src/components/views/Project/Project.js
+++ src/components/views/Project/Project.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container } from "react-bootstrap";
 import styles from "./Project.module.scss";
+import { motion } from "framer-motion";
+
 const Project = () => {
+  const projectVariants = {
+    offscreen: {
+      opacity: 0,
+    },
+    onscreen: {
+      opacity: 1,
+      transition: {
+        duration: 2,
+      },
+    },
+  };
+
   return (
     <section className={styles.sectionProjects}>
       <Container className={styles.container}>
-        <h1 className={styles.titleProject}>Projects</h1>
-        <div className={styles.row}>
+        <h1 className={styles.titleProject}>Original house designs</h1>
+        <motion.div
+          variants={projectVariants}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 1 }}
+          className={styles.row}
+        >
+          {" "}
           <Col xs={10} md={5} lg={4} className={styles.project}>
+            {" "}
             <img
               className={styles.project_img}
               alt="img"
@@ -16,7 +38,7 @@ const Project = () => {
             <p className={styles.project_disc}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               suscipit lobortis risus, in facilisis magna.
-            </p>
+            </p>{" "}
           </Col>
           <Col xs={10} md={5} lg={4} className={styles.project}>
             {" "}
@@ -42,7 +64,7 @@ const Project = () => {
               suscipit lobortis risus, in facilisis magna.
             </p>
           </Col>
-        </div>
+        </motion.div>
       </Container>
     </section>
   );
